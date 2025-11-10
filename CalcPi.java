4edf51b64f67ea1c35576b5c2da93b4f716c3ed6
rony iziagoyev <rony.iziagoyev@post.runi.ac.il>
2025-11-10 23:23:23 +0200

    test1

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,29 @@
-// Computes an approximation of PI.
 public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	public static void main(String [] args) {
+		String[] time = args[0].split(":"); // get time 
+		String minutesToAdd = args[1]; // get minutes to add
+		int Toadd = Integer.parseInt(minutesToAdd);
+		int hours = Integer.parseInt(time[0]);
+		int minutes = Integer.parseInt(time[1]);
+
+		int totalMinutes = hours * 60 + minutes + Toadd; // calculate total minutes
+		int newHours = (totalMinutes / 60) % 24; // calculate new hours
+		int newMinutes = totalMinutes - ((totalMinutes / 60) * 60); // calculate new minutes
+		
+		if (newMinutes < 10 ) { 
+			
+			if(newHours < 10) {
+				System.out.print("0" + newHours + ":0" + newMinutes);
+			}else {
+				System.out.print(newHours + ":0" + newMinutes);
+			}
+		    }else {
+		    	if(newHours < 10) {
+					System.out.print("0" + newHours + ":" + newMinutes);
+				}else {
+					System.out.print(newHours + ":" + newMinutes);
+				}
+		    }
+
 	}
-}
+} 
