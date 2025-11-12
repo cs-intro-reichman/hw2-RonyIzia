81fb8536b63ccaf47095caf1e4e354ce805b7029
rony iziagoyev <rony.iziagoyev@post.runi.ac.il>
2025-11-12 19:41:46 +0200

    agahaha

--- Cheers.java
+++ Cheers.java
@@ -21,7 +21,7 @@ public class Cheers {
                        
                        System.out.println("What does that spell?");
                        for(int j = 0; j < times; j++) { // repeat cheering phrase
-                        System.out.println(phrase + "! ! !");
+                        System.out.println(phrase + "!!!");
                        }
         }
  }
