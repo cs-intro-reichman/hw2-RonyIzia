089c89e9714b6b14a3e9116962b1f712716428fc
rony iziagoyev <rony.iziagoyev@post.runi.ac.il>
2025-11-13 22:05:18 +0200

    final11

--- Collatz.java
+++ Collatz.java
@@ -1,29 +1,35 @@
-// Demonstrates the Collatz conjecture.
 public class Collatz {
-	public static void main(String args[]) {
-		String numero = args[0]; // get starting number
-		int number = Integer.parseInt(numero); 
-		int seed = 1; // starting seed to count until number
-		
-		for(seed = 1; seed <= number; seed++) { // loop from 1 to number
-			int counter = 1; 
-		    System.out.println(); // new line for each seed
-			System.out.print(seed + " "); // print starting seed
-			int currentNumero = seed; // current number in the sequence
-		while (currentNumero != 1) {
-		  if (currentNumero % 2 == 0) { // even
-			  currentNumero /= 2;
-			  System.out.print(currentNumero + " " );}
-			else {
-			  currentNumero = 3 * currentNumero + 1; // odd
-			  System.out.print(currentNumero + " ");
-		       
-			} 
-             counter++; // count the steps
-	    }
-		   System.out.print("(" + counter + ")");
-
-
-	}
-	} 
+    public static void main(String args[]) {
+        int number = Integer.parseInt(args[0]); 
+        String mode = args[1];
+        int seed = 1;
+        
+        if(mode.equals("v")){
+            for(seed = 1; seed <= number; seed++) {
+                int currentNumero = seed;
+                int counter = 0; 
+          
+                System.out.println();
+            
+                do { 
+                    System.out.print(currentNumero + " ");
+                    counter++;
+            
+                    if (currentNumero % 2 == 0) {
+                       currentNumero /= 2;
+                    } else {
+                      currentNumero = 3 * currentNumero + 1;
+                    }
+                } while (currentNumero != 1);
+            
+                System.out.print(currentNumero + " ");
+                counter++;
+             
+                System.out.print("(" + counter + ")");
+            }
+        }
+        
+        System.out.println();
+        // System.out.println("Every one of the first " + number + " hailstone sequences reached 1.");
+    } 
 }
