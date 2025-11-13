b29298fc65851a4ee27d67329349a8dbf5f4f65c
rony iziagoyev <rony.iziagoyev@post.runi.ac.il>
2025-11-13 22:23:14 +0200

    final12

--- Collatz.java
+++ Collatz.java
@@ -1,13 +1,13 @@
 public class Collatz {
     public static void main(String args[]) {
-        int number = Integer.parseInt(args[0]); 
-        String mode = args[1];
+        int number = Integer.parseInt(args[0]);  // starting number
+        String mode = args[1]; // mode: v or c
         int seed = 1;
         
-        if(mode.equals("v")){
-            for(seed = 1; seed <= number; seed++) {
-                int currentNumero = seed;
-                int counter = 0; 
+        if(mode.equals("v")) {                  // verbose mode
+            for(seed = 1; seed <= number; seed++) {         // for each starting number
+                int currentNumero = seed;              
+                int counter = 0;                        // counts how many actions taken
           
                 System.out.println();
             
@@ -15,12 +15,12 @@ public class Collatz {
                     System.out.print(currentNumero + " ");
                     counter++;
             
-                    if (currentNumero % 2 == 0) {
+                    if (currentNumero % 2 == 0) {            // even
                        currentNumero /= 2;
-                    } else {
+                    } else {                                 // not even
                       currentNumero = 3 * currentNumero + 1;
                     }
-                } while (currentNumero != 1);
+                } while (currentNumero != 1);          // repeat until we reach 1
             
                 System.out.print(currentNumero + " ");
                 counter++;
@@ -30,6 +30,6 @@ public class Collatz {
         }
         
         System.out.println();
-        // System.out.println("Every one of the first " + number + " hailstone sequences reached 1.");
+        System.out.println("Every one of the first " + number + " hailstone sequences reached 1.");
     } 
 }
